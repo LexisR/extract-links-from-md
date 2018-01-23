9990d6935b03add569c65d3852a1de2a0ab8fa26
Evelynpa <evelyn.parra.rojas@gmail.com>
2018-01-23 13:00:35 -0300

    prueba de funcionalidad

--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 var fs = require('fs');
 
 fs.readFile('markdown.md', 'utf8', function (err, contents) {
-    var file = contents;
-    // console.log(file);
+  var file = contents;
+  // console.log(file);
 
-    var newRegex = /\(.*?\)/g;
-    var myArray;
-    while ((myArray = newRegex.exec(file)) !== null) { 
-        var msg = myArray[0];
-        var subString = msg.substring(1, msg.length - 1);
-        console.log(subString);
-        // var newArray = [];
-        // newArray.push(subString);
-        //console.log(newArray);
+  var newRegex = /\(.*?\)/g;
+  var myArray;
+  var collection = [];
+  while ((myArray = newRegex.exec(file)) !== null) {
+    var msg = myArray[0];
+    var subString = msg.substring(1, msg.length - 1);
+    // console.log(subString);
+
+  
+    collection[0] = {
+      href : subString
     }
+
+    // var newArray = [];
+    // newArray.push(subString);
+    //console.log(newArray);
+    console.log(collection);
+  }
+  
 });
 module.exports.fs = fs;
 
