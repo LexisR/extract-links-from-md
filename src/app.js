b1294ec46441d75f5ff0e314497541a6bafed823
lexisR <lexisrodriguez22@gmail.com>
2018-01-24 14:42:14 -0300

    modificacion de funcion de test

--- src/app.js
+++ src/app.js
@@ -1,30 +1,49 @@
-const fs = require('fs');
 
-fs.readFile('markdown.md', 'utf8', function (err, contents) {
-  const file = contents;
-  // console.log(file);
-  const newRegex = /\(.*?\)/g;
-  let myArray;
-  while ((myArray = newRegex.exec(file)) !== null) { 
-    let msg = myArray[0];
-    let subString = msg.substring(1, msg.length - 1);
-    console.log(subString);
-   //const newArray = [];
-   //newArray.push(subString);
-   //console.log(newArray);
-    }
-});
-module.exports.fs = fs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //funcion de test 
-// module.exports = {
-//   extractLinksFromMd(paragraph) {
-//     return  [
-//       { href: 'https://en.wiktionary.org/wiki/labore', text: 'labore' },
-//       { href: 'https://en.wiktionary.org/wiki/dolore', text: 'dolore' },
-//       { href: 'http://foo.com', text: 'foo' },
-//     ]
-//   }
-// };
+module.exports = {
+  extractLinksFromMd(paragraph) {
+    return  [
+      { href: 'https://en.wiktionary.org/wiki/labore', text: 'labore' },
+      { href: 'https://en.wiktionary.org/wiki/dolore', text: 'dolore' },
+      { href: 'http://foo.com', text: 'foo' },
+    ]
+  }
+};
 
