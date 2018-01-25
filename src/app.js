58ef1ac15da0c8fc903f5ead694ae82b8556af33
Evelynpa <evelyn.parra.rojas@gmail.com>
2018-01-25 10:17:00 -0300

    resolviendo conflictos

--- src/app.js
+++ src/app.js
@@ -1,50 +1,151 @@
-const fs = require('fs');
-let collection = [];
 
-fs.readFile('markdown.md', 'utf8', function (err, contents) {
 
-  var file = contents;
-  // console.log(file);
-  
-  // const newRegex = /(\(.+?)(\))/g;
-  // const name = /(\[(.*?)\])/g;
-  const newRegex = /(\[(.*?)\]\()(.+?)(\))/g;
-  let myArray;
 
-  while ((myArray = newRegex.exec(file)) !== null) {
-    let msg = myArray[0];
-    let lastText = ']'.charCodeAt(0);
-    let subStringText = msg.substring(1, msg.length - 1);
-    let subStringLink = msg.substring(1, subStringText + 2);
-  
-    console.log(lastText);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const fs = require('fs');
+// let collection = [];
+
+//  fs.readFile('markdown.md', 'utf8', function (err, contents) {
+
+//   var file = contents;
+//   const newRegex = /(\[(.*?)\]\()(.+?)(\))/g;
+//   let myArray;
+//   let linkArray = [];
+//   while ((myArray = newRegex.exec(file)) !== null) {
+//     let textLink = myArray[2];
+//     let linkUrl = myArray[3];
+//     var link2 = new LinksFromMd(linkUrl, textLink);
     
-
-    // collection[0] = {
-    //   href: JSON.stringify(subString)
-
-    // }
-    // console.log(msg.split(' '));
-  }
-
-});
-class ExtractLinksFromMd {
-  constructor(link, text) {
-    this.href = link;
-    this.text = text;
-  }
-}
-//module.exports.fs = fs;
+//     linkArray.push(link2);
+    
+  
+//   }
+  
+//   console.log(linkArray);
+// });
 
 
-//funcion de test 
+
+// class LinksFromMd {
+//   constructor(link, text) {
+//     this.href = link;
+//     this.text = text;
+//   }
+// }
+// module.exports.fs = fs;
+
+
+// funcion de test 
 // module.exports = {
-//   extractLinksFromMd(paragraph) {
-//     return  [
-//       { href: 'https://en.wiktionary.org/wiki/labore', text: 'labore' },
-//       { href: 'https://en.wiktionary.org/wiki/dolore', text: 'dolore' },
-//       { href: 'http://foo.com', text: 'foo' },
-//     ]
+//   extractLinksFromMd(file) {
+
 //   }
 // };
 
